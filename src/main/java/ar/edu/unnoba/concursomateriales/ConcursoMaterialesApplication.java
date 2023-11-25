bde690ecb66cd1382b01817127be949f28c4f514
AlexisMahuad <alexismahuad2052@gmail.com>
2023-11-25 12:15:41 -0300

    Estructura inicial del proyecto

---
+++ src/main/java/ar/edu/unnoba/concursomateriales/ConcursoMaterialesApplication.java
@@ -0,0 +1,13 @@
+package ar.edu.unnoba.concursomateriales;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConcursoMaterialesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConcursoMaterialesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ar/edu/unnoba/concursomateriales/ConcursoMaterialesApplicationTests.java
@@ -0,0 +1,13 @@
+package ar.edu.unnoba.concursomateriales;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConcursoMaterialesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
